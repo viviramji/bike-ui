69017aea640cbbc55efb20e1b262c783e0a19934
Victor Victor RJ <wrjr04@gmail.com>
2023-02-24 20:35:58 -0500

    Add form controller method.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,25 +12,38 @@ export class HomeComponent {
   models: String[] = [
     'model 1', 'model 2', 'model 3'
   ];
-  bikeForm: FormGroup;
+
+  bikeForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+    model: new FormControl('', Validators.required),
+    serialNumber: new FormControl('', Validators.required),
+    purchasePrice: new FormControl('', Validators.required),
+    purchaseDate: new FormControl('', Validators.required),
+    conact: new FormControl(),
+  });
+
   validMessage: String = '';
 
   constructor(private bikeService: BikeService) { }
 
   ngOnInit() {
-    this.bikeForm = new FormGroup({
-      name: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
-      phone: new FormControl('', Validators.required),
-      model: new FormControl('', Validators.required),
-      serialNumber: new FormControl('', Validators.required),
-      purchasePrice: new FormControl('', Validators.required),
-      purchaseDate: new FormControl('', Validators.required),
-      conact: new FormControl(),
-    });
+    
   }
 
   submitRegistration() {
-
+    if (this.bikeForm.valid) {
+      this.validMessage = "Your bike registration has been submitted. Thank you!";
+      this.bikeService.createBikeRegistration(this.bikeForm.value).subscribe({
+        next: (data) => {
+          this.bikeForm.reset();
+          return true;
+        },
+        error: (e) => console.error(e),
+      })
+    } else {
+      this.validMessage = "Please fill out the form before submitting!";
+    }
   }
 }
